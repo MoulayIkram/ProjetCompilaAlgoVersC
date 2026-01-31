b1e23acb16b374d2babf525abbbadb1f42862485
EL BOJADDAINI Ouidad <elbojaddainiouidad@gmail.com>
2026-01-31 02:09:25 +0100

    Update tokens.h

--- tokens.h
+++ tokens.h
@@ -3,173 +3,103 @@
 
 #include <stdbool.h>
 
-// Enumération des types de tokens (normaux et erreurs)
+// Énumération des tokens (normaux et erreur)
 typedef enum {
-    // === Mots-clés de structure ===
-    TOK_ALGORITHME,          // ALG_TOKEN
-    TOK_ALG_ERR,            // ALG_ERR
-    TOK_VARIABLES,           // VARS_TOKEN
-    TOK_VARS_ERR,           // VARS_ERR
-    TOK_DEBUT,               // BEGIN_TOKEN
-    TOK_BEGIN_ERR,          // BEGIN_ERR
-    TOK_FIN,                 // END_TOKEN
-    TOK_END_ERR,            // END_ERR
-    
-    // === Déclarations, types et constantes ===
-    TOK_VARIABLE,            // VAR_TOKEN
-    TOK_VAR_ERR,            // VAR_ERR
-    TOK_CONSTANTE,           // CONST_TOKEN
-    TOK_CONST_ERR,          // CONST_ERR
-    TOK_ENTIER,              // INT_TYPE_TOKEN
-    TOK_INT_TYPE_ERR,       // INT_TYPE_ERR
-    TOK_REEL,                // REAL_TYPE_TOKEN
-    TOK_REAL_TYPE_ERR,      // REAL_TYPE_ERR
-    TOK_CARACTERE,           // CHAR_TYPE_TOKEN
-    TOK_CHAR_TYPE_ERR,      // CHAR_TYPE_ERR
-    TOK_CHAINE,              // STRING_TYPE_TOKEN
-    TOK_STRING_TYPE_ERR,    // STRING_TYPE_ERR
-    TOK_BOOLEEN,             // BOOL_TYPE_TOKEN
-    TOK_BOOL_TYPE_ERR,      // BOOL_TYPE_ERR
-    TOK_TABLEAU,             // ARRAY_TOKEN
-    TOK_ARRAY_ERR,          // ARRAY_ERR
-    TOK_DE,                  // OF_TOKEN
-    TOK_OF_ERR,             // OF_ERR
-    TOK_STRUCTURE,           // STRUCT_TOKEN
-    TOK_STRUCT_ERR,         // STRUCT_ERR
-    TOK_FIN_STRUCT,          // ENDSTRUCT_TOKEN
-    TOK_ENDSTRUCT_ERR,      // ENDSTRUCT_ERR
-    
-    // === Entrées/Sorties ===
-    TOK_ECRIRE,              // WRITE_TOKEN
-    TOK_WRITE_ERR,          // WRITE_ERR
-    TOK_LIRE,                // READ_TOKEN
-    TOK_READ_ERR,           // READ_ERR
-    TOK_RETOUR,              // NEWLINE_TOKEN
-    TOK_NEWLINE_ERR,        // NEWLINE_ERR
-    
-    // === Constantes logiques et opérateurs ===
-    TOK_VRAI,                // TRUE_TOKEN
-    TOK_TRUE_ERR,           // TRUE_ERR
-    TOK_FAUX,                // FALSE_TOKEN
-    TOK_FALSE_ERR,          // FALSE_ERR
-    TOK_ET,                  // AND_TOKEN
-    TOK_AND_ERR,            // AND_ERR
-    TOK_OU,                  // OR_TOKEN
-    TOK_OR_ERR,             // OR_ERR
-    TOK_NON,                 // NOT_TOKEN
-    TOK_NOT_ERR,            // NOT_ERR
-    
-    // === Comparateurs ===
-    TOK_INFERIEUR,           // LT_TOKEN
-    TOK_LT_ERR,             // LT_ERR
-    TOK_INFERIEUR_EGAL,      // LE_TOKEN
-    TOK_LE_ERR,             // LE_ERR
-    TOK_SUPERIEUR,           // GT_TOKEN
-    TOK_GT_ERR,             // GT_ERR
-    TOK_SUPERIEUR_EGAL,      // GE_TOKEN
-    TOK_GE_ERR,             // GE_ERR
-    TOK_EGAL,                // EQ_TOKEN
-    TOK_EQ_ERR,             // EQ_ERR
-    TOK_DIFFERENT,           // NEQ_TOKEN
-    TOK_NEQ_ERR,            // NEQ_ERR
-    
-    // === Affectation, séparateurs, ponctuation ===
-    TOK_AFFECTATION,         // ASSIGN_TOKEN
-    TOK_ASSIGN_ERR,         // ASSIGN_ERR
-    TOK_DEUX_POINTS,         // COLON_TOKEN
-    TOK_COLON_ERR,          // COLON_ERR
-    TOK_VIRGULE,             // COMMA_TOKEN
-    TOK_COMMA_ERR,          // COMMA_ERR
-    TOK_PAREN_OUVRANTE,      // LPAREN_TOKEN
-    TOK_LPAREN_ERR,         // LPAREN_ERR
-    TOK_PAREN_FERMANTE,      // RPAREN_TOKEN
-    TOK_RPAREN_ERR,         // RPAREN_ERR
-    TOK_CROCHET_OUVRANT,     // LBRACK_TOKEN
-    TOK_LBRACK_ERR,         // LBRACK_ERR
-    TOK_CROCHET_FERMANT,     // RBRACK_TOKEN
-    TOK_RBRACK_ERR,         // RBRACK_ERR
-    TOK_POINTS,              // DOTDOT_TOKEN
-    TOK_DOTDOT_ERR,         // DOTDOT_ERR
-    
-    // === Opérateurs arithmétiques ===
-    TOK_PLUS,                // PLUS_TOKEN
-    TOK_PLUS_ERR,           // PLUS_ERR
-    TOK_MOINS,               // MINUS_TOKEN
-    TOK_MINUS_ERR,          // MINUS_ERR
-    TOK_FOIS,                // MUL_TOKEN
-    TOK_MUL_ERR,            // MUL_ERR
-    TOK_DIVISE,              // DIV_TOKEN
-    TOK_DIV_ERR,            // DIV_ERR
-    TOK_DIV_ENTIER,          // INTDIV_TOKEN
-    TOK_INTDIV_ERR,         // INTDIV_ERR
-    TOK_MODULO,              // MOD_TOKEN
-    TOK_MOD_ERR,            // MOD_ERR
-    TOK_PUISSANCE,           // POW_TOKEN
-    TOK_POW_ERR,            // POW_ERR
-    
-    // === Structures de contrôle ===
-    TOK_SI,                  // IF_TOKEN
-    TOK_IF_ERR,             // IF_ERR
-    TOK_ALORS,               // THEN_TOKEN
-    TOK_THEN_ERR,           // THEN_ERR
-    TOK_SINON,               // ELSE_TOKEN
-    TOK_ELSE_ERR,           // ELSE_ERR
-    TOK_FIN_SI,              // ENDIF_TOKEN
-    TOK_ENDIF_ERR,          // ENDIF_ERR
-    TOK_SELON,               // SWITCH_TOKEN
-    TOK_SWITCH_ERR,         // SWITCH_ERR
-    TOK_FIN_SELON,           // ENDSWITCH_TOKEN
-    TOK_ENDSWITCH_ERR,      // ENDSWITCH_ERR
-    TOK_SORTIR,              // BREAK_SWITCH_TOKEN
-    TOK_BREAK_SWITCH_ERR,   // BREAK_SWITCH_ERR
-    TOK_POUR,                // FOR_TOKEN
-    TOK_FOR_ERR,            // FOR_ERR
-    TOK_JUSQUA,              // TO_TOKEN
-    TOK_TO_ERR,             // TO_ERR
-    TOK_PAS,                 // STEP_TOKEN
-    TOK_STEP_ERR,           // STEP_ERR
-    TOK_FIN_POUR,            // ENDFOR_TOKEN
-    TOK_ENDFOR_ERR,         // ENDFOR_ERR
-    TOK_QUITTER_POUR,        // BREAK_FOR_TOKEN
-    TOK_BREAK_FOR_ERR,      // BREAK_FOR_ERR
-    TOK_TANTQUE,             // WHILE_TOKEN
-    TOK_WHILE_ERR,          // WHILE_ERR
-    
-    // === Identifiants et littéraux ===
-    TOK_IDENTIFIANT,
-    TOK_NOMBRE_ENTIER,
-    TOK_NOMBRE_REEL,
-    TOK_CHAINE_LITTERALE,
-    TOK_CARACTERE_LITTERAL,
+    // 1. Mots-clés de structure
+    TOK_ALGORITHME, TOK_ALGORITHME_ERR,
+    TOK_DEBUT, TOK_DEBUT_ERR,
+    TOK_FIN, TOK_FIN_ERR,
 
-    // === Nouveaux tokens ajoutés ===
-    TOK_GUILLEMET,           // QUOTE_TOKEN
-    TOK_GUILLEMET_ERR,       // QUOTE_ERR
-    TOK_ID,                  // ID_TOKEN
-    TOK_ID_ERR,              // ID_ERR
-    TOK_CONST_ENTIERE,       // INT_CONST_TOKEN
-    TOK_CONST_ENTIERE_ERR,   // INT_CONST_ERR
-    TOK_CONST_REEL,          // REAL_CONST_TOKEN
-    TOK_CONST_REEL_ERR,      // REAL_CONST_ERR
-    TOK_CONST_CHAINE,        // STRING_CONST_TOKEN
-    TOK_CONST_CHAINE_ERR,    // STRING_CONST_ERR
-    TOK_PROCEDURE,           // PROCEDURE_TOKEN
-    TOK_PROCEDURE_ERR,       // PROCEDURE_ERR
-    TOK_FIN_PROC,            // END_PROCEDURE_TOKEN
-    TOK_FIN_PROC_ERR,        // END_PROCEDURE_ERR
-    TOK_FONCTION,            // FUNCTION_TOKEN
-    TOK_FONCTION_ERR,        // FUNCTION_ERR
-    TOK_FIN_FONCT,           // END_FUNCTION_TOKEN
-    TOK_FIN_FONCT_ERR,       // END_FUNCTION_ERR
-    TOK_RETOURNER,           // RETURN_TOKEN
-    TOK_RETOURNER_ERR,       // RETURN_ERR
-    TOK_REPETER,             // REPEAT_TOKEN
-    TOK_REPETER_ERR,         // REPEAT_ERR
-    
-    // === Spéciaux ===
-    TOK_COMMENTAIRE,
-    TOK_ERREUR_GENERIQUE,
-    TOK_EOF
+    // 2. Déclarations, types et constantes
+    TOK_OBJETS, TOK_OBJETS_ERR,
+    TOK_VARIABLE, TOK_VARIABLE_ERR,
+    TOK_CONSTANTE, TOK_CONSTANTE_ERR,
+    TOK_ENTIER, TOK_ENTIER_ERR,
+    TOK_REEL, TOK_REEL_ERR,
+    TOK_CARACTERE, TOK_CARACTERE_ERR,
+    TOK_CHAINE, TOK_CHAINE_ERR,
+    TOK_BOOLEEN, TOK_BOOLEEN_ERR,
+    TOK_CONST_ENTIERE, TOK_CONST_ENTIERE_ERR,
+    TOK_CONST_REEL, TOK_CONST_REEL_ERR,
+    TOK_CONST_CHAINE, TOK_CONST_CHAINE_ERR,
+    TOK_ID, TOK_ID_ERR,
+    TOK_TABLEAU, TOK_TABLEAU_ERR,
+    TOK_DE, TOK_DE_ERR,
+    TOK_STRUCTURE, TOK_STRUCTURE_ERR,
+    TOK_FIN_STRUCT, TOK_FIN_STRUCT_ERR,
+
+    // 3. Entrées / sorties
+    TOK_ECRIRE, TOK_ECRIRE_ERR,
+    TOK_LIRE, TOK_LIRE_ERR,
+    TOK_RETOUR, TOK_RETOUR_ERR,
+
+    // 4. Constantes logiques et opérateurs logiques
+    TOK_VRAI, TOK_VRAI_ERR,
+    TOK_FAUX, TOK_FAUX_ERR,
+    TOK_ET, TOK_ET_ERR,
+    TOK_OU, TOK_OU_ERR,
+    TOK_NON, TOK_NON_ERR,
+
+    // 5. Comparateurs
+    TOK_INFERIEUR, TOK_INFERIEUR_ERR,
+    TOK_INFERIEUR_EGAL, TOK_INFERIEUR_EGAL_ERR,
+    TOK_SUPERIEUR, TOK_SUPERIEUR_ERR,
+    TOK_SUPERIEUR_EGAL, TOK_SUPERIEUR_EGAL_ERR,
+    TOK_EGAL, TOK_EGAL_ERR,
+    TOK_DIFFERENT, TOK_DIFFERENT_ERR,
+
+    // 6. Affectation, séparateurs, ponctuation
+    TOK_AFFECTATION, TOK_AFFECTATION_ERR,
+    TOK_DEUX_POINTS, TOK_DEUX_POINTS_ERR,
+    TOK_VIRGULE, TOK_VIRGULE_ERR,
+    TOK_PAREN_OUVRANTE, TOK_PAREN_OUVRANTE_ERR,
+    TOK_PAREN_FERMANTE, TOK_PAREN_FERMANTE_ERR,
+    TOK_CROCHET_OUVRANT, TOK_CROCHET_OUVRANT_ERR,
+    TOK_CROCHET_FERMANT, TOK_CROCHET_FERMANT_ERR,
+    TOK_GUILLEMET, TOK_GUILLEMET_ERR,
+    TOK_POINT, TOK_POINT_ERR,
+    TOK_FIN_INSTR, TOK_FIN_INSTR_ERR,
+
+    // 7. Opérateurs arithmétiques
+    TOK_PLUS, TOK_PLUS_ERR,
+    TOK_MOINS, TOK_MOINS_ERR,
+    TOK_FOIS, TOK_FOIS_ERR,
+    TOK_DIVISE, TOK_DIVISE_ERR,
+    TOK_DIV_ENTIER, TOK_DIV_ENTIER_ERR,
+    TOK_MODULO, TOK_MODULO_ERR,
+    TOK_PUISSANCE, TOK_PUISSANCE_ERR,
+
+    // 8. Structures de contrôle
+    TOK_SI, TOK_SI_ERR,
+    TOK_SINONSI, TOK_SINONSI_ERR,
+    TOK_ALORS, TOK_ALORS_ERR,
+    TOK_SINON, TOK_SINON_ERR,
+    TOK_FIN_SI, TOK_FIN_SI_ERR,
+    TOK_SELON, TOK_SELON_ERR,
+    TOK_CAS, TOK_CAS_ERR,
+    TOK_DEFAUT, TOK_DEFAUT_ERR,
+    TOK_FIN_SELON, TOK_FIN_SELON_ERR,
+    TOK_SORTIR, TOK_SORTIR_ERR,
+    TOK_POUR, TOK_POUR_ERR,
+    TOK_JUSQUA, TOK_JUSQUA_ERR,
+    TOK_REPETER, TOK_REPETER_ERR,
+    TOK_PAS, TOK_PAS_ERR,
+    TOK_FIN_POUR, TOK_FIN_POUR_ERR,
+    TOK_QUITTER_POUR, TOK_QUITTER_POUR_ERR,
+    TOK_TANTQUE, TOK_TANTQUE_ERR,
+    TOK_FINTANTQUE, TOK_FINTANTQUE_ERR,
+
+    // 9. Procédures et fonctions
+    TOK_PROCEDURE, TOK_PROCEDURE_ERR,
+    TOK_FIN_PROC, TOK_FIN_PROC_ERR,
+    TOK_FONCTION, TOK_FONCTION_ERR,
+    TOK_FIN_FONCT, TOK_FIN_FONCT_ERR,
+    TOK_RETOURNER, TOK_RETOURNER_ERR,
+
+    // 10. Autres tokens spéciaux
+    TOK_EOF, TOK_EOF_ERR,
+    TOK_COMMENTAIRE, TOK_COMMENTAIRE_ERR,
+    TOK_COMMENTAIRES, TOK_COMMENTAIRES_ERR
 } TokenType;
 
 // Structure d'un token
@@ -178,12 +108,30 @@ typedef struct {
     char* valeur;
     int ligne;
     int colonne;
-    bool est_erreur;  // Indique si c'est un token d'erreur
 } Token;
 
-// Fonctions pour les tokens
-const char* token_type_to_string(TokenType type);
-bool est_token_erreur(TokenType type);
-void afficher_token(Token* token);
+// Prototypes des fonctions
+Token* creer_token(TokenType type, const char* valeur, int ligne, int colonne);
+void detruire_token(Token* token);
+Token* copier_token(const Token* token);
 
-#endif // TOKENS_H
+const char* token_to_string(TokenType type);
+void afficher_token_ligne(const Token* token);
+void afficher_token_compact(const Token* token);
+void afficher_token_detail(const Token* token);
+
+bool est_token_erreur(TokenType type);
+bool est_mot_cle(TokenType type);
+bool est_operateur(TokenType type);
+bool est_separateur(TokenType type);
+bool est_constante(TokenType type);
+bool est_type_donnee(TokenType type);
+
+bool tokens_egaux(const Token* t1, const Token* t2);
+bool tokens_types_egaux(const Token* t1, const Token* t2);
+bool tokens_valeurs_egales(const Token* t1, const Token* t2);
+
+bool token_valeur_vide(const Token* token);
+bool token_valide(const Token* token);
+
+#endif
