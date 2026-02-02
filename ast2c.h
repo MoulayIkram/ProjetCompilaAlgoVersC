31c5a2c0d5fb6427587327c8ceb991b0a026f610
EL BOJADDAINI Ouidad <elbojaddainiouidad@gmail.com>
2026-02-02 15:28:42 +0100

    Remove ast2c.h header file

--- ast2c.h
+++ ast2c.h
@@ -1,15 +1,1 @@
-#ifndef AST2C_H
-#define AST2C_H
 
-#include <stdio.h>
-#include <stdbool.h>
-
-#include "ast.h"
-
-// Génère du C vers un flux (stdout ou fichier déjà ouvert)
-bool ast2c_generate_stream(ASTNode* program, FILE* out);
-
-// Génère du C vers un fichier (crée/écrase)
-bool ast2c_generate_file(ASTNode* program, const char* out_c_path);
-
-#endif
